ec49634d64256a58446fe79cc0a10bbf62149273
g091 <gotieno791@gmail.com>
2022-07-02 01:28:45 +0300

    txt indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that prints a text with 2 new lines after each of these characters: ., ? and :"""
+
+
+def text_indentation(text):
+    """Prints 2 lines after . or : or ?"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
