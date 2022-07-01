f0bdc1063c8e2431828ea36c3f6ffffdc3278d9c
g091 <gotieno791@gmail.com>
2022-07-01 23:04:32 +0300

    t1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Represents a class Square."""
+
+
+class Square:
+    """Class Method."""
+
+    def __init__(self, size):
+        """Initializing the data."""
+        self.__size = size
