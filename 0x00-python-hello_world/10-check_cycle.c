cb86bf8678bcfd552c7167d73c147f6b73267b1b
g091 <gotieno791@gmail.com>
2022-07-01 22:25:32 +0300

    t10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a linked list cycles or not
+ * @list: linked list
+ * Return: 1 if cycle, 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *s1 = NULL, *s2 = NULL;
+
+	s1 = s2 = list;
+	while (list && s1 && s2 && s1->next && s2->next)
+	{
+		s1 = s1->next;
+		s2 = s2->next->next;
+		if (!s2 || !s1)
+			return (0);
+		if (s2->next == s1)
+			return (1);
+	}
+	return (0);
+}
