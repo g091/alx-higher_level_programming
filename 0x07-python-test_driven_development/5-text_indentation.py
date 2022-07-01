2f0401947bf3d827ab7b5392619d1312d5a7c2f9
g091 <gotieno791@gmail.com>
2022-07-02 02:13:31 +0300

    txt indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module that prints a text with 2 new lines after each of these characters: ., ? and :"""
+"""Module that prints a text with 2 new lines after each
+of these characters: ., ? and :"""
 
 
 def text_indentation(text):
