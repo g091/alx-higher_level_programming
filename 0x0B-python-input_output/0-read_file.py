90c73e959fbb43f98412eb79ab7132979c630f67
g091 <gotieno791@gmail.com>
2022-07-07 00:49:24 +0300

    t0_Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Reading the file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
