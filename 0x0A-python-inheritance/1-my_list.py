ec3a58e9764f2942ae672971d9712c5b29ff3313
g091 <gotieno791@gmail.com>
2022-07-06 16:22:33 +0300

    del

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Module that contains the MyList class"""
-
-
-class MyList(list):
-    """Sublass that inherits from the list"""
-    def print_sorted(self):
-        """Prints the list in a sorted order"""
-        print(sorted(self))
