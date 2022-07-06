bdf782bd2225210bf661826f000472056e45ff5c
g091 <gotieno791@gmail.com>
2022-07-07 00:56:55 +0300

    t1_Write to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module that writes a string to a text file (UTF8)
+and returns the number of characters written:"""
+
+
+def write_file(filename="", text=""):
+    """Writing file"""
+    with open(filename, "w", encoding='utf-8') as f:
+        return f.write(text)
