814304d06b52d9196ba397a1224344cba19e74be
g091 <gotieno791@gmail.com>
2022-08-12 03:05:55 +0300

    t18_temp #0

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,2 @@
+-- Script thatdisplays the average temperature (Fahrenheit) by city ordered by temperature
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
