98f6a59b1d5e3823e8bbd0530bfaa5cdf63a1d5e
g091 <gotieno791@gmail.com>
2022-09-08 02:12:12 +0300

    t2_Args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
