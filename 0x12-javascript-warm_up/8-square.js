043fdfc4f2b565d0b329dc60a0536b362ec6e7a0
g091 <gotieno791@gmail.com>
2022-09-08 02:23:03 +0300

    t8_Square size

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else if (process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  const x = Number(process.argv[2]);
+
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
+}
