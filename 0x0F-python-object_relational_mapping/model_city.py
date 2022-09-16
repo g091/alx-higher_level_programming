9935b6cfa9550e7973c8a0bf22345335e2c4ac23
g091 <gotieno791@gmail.com>
2022-09-17 02:36:35 +0300

    t14_p2

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Definition of  State model """
+
+from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+class City(Base):
+    """ Definition of  a class State to be linked to db table """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, nullable=False,
+                autoincrement=True, unique=True,
+                primary_key=True)
+
+    name = Column(String(128), nullable=False)
+
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
