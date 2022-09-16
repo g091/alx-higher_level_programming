af0adb49cea760c967ce68f5704708789b97d98b
g091 <gotieno791@gmail.com>
2022-09-17 01:02:20 +0300

    t0_all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Lists all states from the database hbtn_0e_0_usa"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db_user = argv[1]
+    db_passwd = argv[2]
+    db_name = argv[3]
+
+    database = MySQLdb.connect(host='localhost',port=3306,
+                               user=db_user,passwd=db_passwd,db=db_name)
+
+    cursor = database.cursor()
+
+    cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
+
+    for row in cursor.fetchall():
+        print(row)
