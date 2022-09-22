117231ac99da97957375fee59a705852d04925da
g091 <gotieno791@gmail.com>
2022-09-23 02:45:42 +0300

    t4_cURL header

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # That takes in a URL as an arg, sends a GET request to it, & displays the body
-curl -s "$1" -X GET -H "X-School-User-Id: 98"
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
