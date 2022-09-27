dd22be701a7e8769f181cd1a51eb0ec9b0cd4461
g091 <gotieno791@gmail.com>
2022-09-28 01:44:34 +0300

    t6_POST #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that takes in a URL and an email address, sends a
+POST request to the passed URL with the email as a parameter,
+& finally displays the body of the response."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+    body = requests.post(url, data=values)
+    print(body.text)
