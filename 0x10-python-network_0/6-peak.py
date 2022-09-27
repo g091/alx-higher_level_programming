b5d9278e87634b1678868f743b198e060e36609e
g091 <gotieno791@gmail.com>
2022-09-28 02:23:03 +0300

    t6_Peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Function that finds a peak in a list of unsorted integers"""
+"""Function that finds a peak in a list of unsorted integers."""
 
 
 def find_peak(list_of_integers):
     """Finds the peak"""
 
-    A = list_of_integers[:]
-    if A == []:
+    if list_of_integers == []:
         return None
 
-    def recursive(A, left=0, right=len(A) - 1):
-        """Recursive function"""
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        return max(list_of_integers)
 
-        mid = (left + right) // 2
-        if ((mid == 0 or A[mid - 1] <= A[mid]) and
-                (mid == len(A) - 1 or A[mid + 1] <= A[mid])):
-            return A[mid]
-        if mid - 1 >= 0 and A[mid - 1] > A[mid]:
-            return recursive(A, left, mid - 1)
-        return recursive(A, mid + 1, right)
-    return recursive(A)
+    mid = len(list_of_integers) // 2
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
